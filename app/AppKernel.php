02a1027ea5ea58fa69db178df9ab4311558046ae
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-04-04 13:32:05 +0200

    Chapter 5: The Flex composer.json File

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,8 +28,8 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            // $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            // $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new WebServerBundle();
         }
